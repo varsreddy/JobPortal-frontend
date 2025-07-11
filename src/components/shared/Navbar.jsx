5baa731e1e3b1ccbed29f330f368056b17948020
varsreddy <varshithak809@gmail.com>
2025-07-11 22:28:10 +0530

    navbar updeate

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -44,7 +44,8 @@ const Navbar = () => {
       <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
         <div>
           <h1 className="text-2xl font-bold">
-            Job<span className="text-[#f83002]">Portal</span>
+            <span className="text-[#7209b7]">Job</span>
+            <span className="text-[#f83002]">ify</span>
           </h1>
         </div>
         <div className="flex items-center gap-5">
